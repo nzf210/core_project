259fe5c286e235ed5347ab93ceb4d0908bf9d941
nzf210 <syahrilsymd@gmail.com>
2026-06-29 12:34:53 +0700

    modified:   frontend/umkm-web/src/main.ts 	modified:   frontend/umkm-web/vite.config.ts

--- frontend/umkm-web/vite.config.ts
+++ frontend/umkm-web/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { Unhead } from '@unhead/vue/bundler'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Unhead().vite()],
 })
